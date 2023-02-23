e16320b78e884b3f7be5a1b69fedf1db55111903
AvasBrahma <avasbrahma@gmail.com>
2023-02-24 01:51:23 +0530

    added layout and header html elements

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home=function(req, res){
     
-    return res.end('<h1>Hey Man</h1>');
+    return res.render('home',{
+        title: "home"
+    })
 }
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express=require('express');
 const app=express();
 const port=8001;
+const expressLayouts=require('express-ejs-layouts');
+
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use(expressLayouts);
+
+
 
 
 app.use('/', require('./routes'));
